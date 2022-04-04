6c3eb104c953bd2f1e4abbad9e4d64b96d796bfd
Matt K <matthedud@hotmail.com>
2022-04-04 15:28:31 +0200

    finish

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProfileImage = () => {
+  return (
+    <img
+        src="https://i.imgur.com/9yw1Fyw.jpg"
+        className="profile"
+        alt="profile"
+      />
+  )
+}
+
+export default ProfileImage
---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <span className="user">
+    <span className="name">Ironhack</span>
+    <span className="handle">@ironhack</span>
+  </span>
+  )
+}
+
+export default User
